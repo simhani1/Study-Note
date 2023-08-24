84d7826a5f22a7a911dbab29edeadfe8fa496a79
simhani1 <simhani1@gmail.com>
2023-08-24 21:19:36 +0900

    feat: 할인 적용 메서드를 movie 클래스에 작성한다

--- movie-system/src/main/java/com/theater/movie/Movie.java
+++ movie-system/src/main/java/com/theater/movie/Movie.java
@@ -20,6 +20,27 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
+    public Money calculateAmountDiscountFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee;
+    }
+
     public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
         for (DiscountCondition condition : discountConditions) {
             if (condition.getType() == DiscountConditionType.PERIOD) {
