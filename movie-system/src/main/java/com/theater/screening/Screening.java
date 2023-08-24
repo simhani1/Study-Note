3b928b656b4fe78d5b2d2c69e31517aad00df943
simhani1 <simhani1@gmail.com>
2023-08-24 21:31:35 +0900

    feat: 할인 금액을 계산하는 메서드를 screening 클래스에 추가한다

--- movie-system/src/main/java/com/theater/screening/Screening.java
+++ movie-system/src/main/java/com/theater/screening/Screening.java
@@ -1,5 +1,6 @@
 package com.theater.screening;
 
+import com.theater.money.Money;
 import com.theater.movie.Movie;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,4 +14,28 @@ public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountFee().times(audienceCount);
+        }
+        return movie.calculateNoneDiscountFee().times(audienceCount);
+    }
 }
