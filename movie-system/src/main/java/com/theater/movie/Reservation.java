ee82818af9950a36aa740a89efa254c91fce5c67
simhani1 <simhani1@gmail.com>
2023-08-24 17:45:31 +0900

    feat: reservation 클래스를 추가한다

---
+++ movie-system/src/main/java/com/theater/movie/Reservation.java
@@ -0,0 +1,21 @@
+package com.theater.movie;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Reservation {
+
+    private Customer customer;
+    private Screening screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.screening = screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
+}
