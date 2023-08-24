1b37aaebe7216dc541bb85c636a0bf264a9cc324
simhani1 <simhani1@gmail.com>
2023-08-24 17:41:55 +0900

    feat: 할인 조건 클래스를 추가한다

--- movie-system/src/main/java/com/theater/movie/DiscountCondition.java
+++ movie-system/src/main/java/com/theater/movie/DiscountCondition.java
@@ -1,4 +1,20 @@
 package com.theater.movie;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Getter
+@Setter
 public class DiscountCondition {
+
+    private DiscountConditionType discountConditionType;
+
+    private int sequence;
+
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
 }
